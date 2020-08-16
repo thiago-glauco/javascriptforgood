ef494accf283d4608b2e9db0c2acc12d5f506877
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-08-16 16:06:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascriptforgood
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/javascriptforgood)
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material/material.module';
+
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports:      [ 
+    BrowserModule, BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule
+  ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
